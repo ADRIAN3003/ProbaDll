4753a4b10c876c88298a102bb8c85dc5f605943a
Adrián Medve <medveadrian03@gmail.com>
2022-11-16 13:43:06 +0100

    Add project files.

---
+++ ProbaDll/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProbaDll
+{
+    public class Class1
+    {
+    }
+}
