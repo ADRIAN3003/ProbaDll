3f94809074700204fb6e2745032398f7bc5f40b5
Adrián Medve <medveadrian03@gmail.com>
2022-11-16 13:46:25 +0100

    create Osszead

---
+++ ProbaDll/Szamol.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProbaDll
+{
+    public class Szamol
+    {
+        /// <summary>
+        /// A <paramref name="x"/> és a <paramref name="y"/> egész számok összegét adja.
+        /// </summary>
+        /// <param name="x">Első egész operandus</param>
+        /// <param name="y">Második egész operandus</param>
+        /// <returns><paramref name="x"/>+<paramref name="y"/></returns>
+        public int Osszead(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
