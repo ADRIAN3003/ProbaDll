43c777567177dce7e9b8a2f99c188632539f7a50
Adrián Medve <medveadrian03@gmail.com>
2022-11-16 13:54:34 +0100

    test the dll

--- Kiprobalas/Program.cs
+++ Kiprobalas/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ProbaDll;
 
 namespace Kiprobalas
 {
@@ -10,6 +11,12 @@ namespace Kiprobalas
     {
         static void Main(string[] args)
         {
+            Szamol sz = new Szamol();
+
+            Console.WriteLine(sz.Osszead(3, 2));
+            Console.WriteLine(sz.Szorzat(4, 12));
+
+            Console.ReadKey();
         }
     }
 }
